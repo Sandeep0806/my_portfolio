c4b66f3a17a9d495f50d17d2070334ba556d6829
sandeep0806 <kokkondasandeep10@gmail.com>
2025-08-28 16:25:36 +0530

    profile

--- src/components/ProfilePicture.jsx
+++ src/components/ProfilePicture.jsx
@@ -2,7 +2,7 @@ export default function ProfilePicture() {
   return (
     <div className="relative w-60 h-60 mx-auto rounded-full neon-glow">
       <img
-        src="/profile.png" // replace with your profile image path
+        src="/profile.jpg" // replace with your profile image path
         alt="Profile"
         className="rounded-full object-cover w-full h-full"
       />
